edadf7d5fa0b6947969f28f7561de83fedeef958
Theodor <54949643+theodorerlax@users.noreply.github.com>
2021-02-18 11:04:52 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.body.style.backgroundColor = "pink";
 
 //1
-const value = +prompt("Introduceti nr", '');
+/*const value = +prompt("Introduceti nr", '');
 if (value > 0) {
     alert("1");
 } else if (value < 0) {
@@ -23,12 +23,12 @@ if (!(age >= 14 && age <= 90)) {
     alert('False');
     else
         alert('True');
-}
+}*/
 
 
 
 //3
-const username = prompt("Enter username:");
+/*const username = prompt("Enter username:");
 const password = prompt("Enter password:");
 
 function validationAccount(name, pass) {
@@ -47,14 +47,34 @@ function validationAccount(name, pass) {
         alert("Incorrect username and password!");
 
     }
-}
+}*/
 
-validationAccount('admin', '123');
+// validationAccount('admin', '123');
 //4
-const a = prompt('Enter min');
+/*const a = prompt('Enter min');
 const b = prompt('Enter max');
 for (let i = a; i <= b; i++) {
     if (i % 2 == 0) {
         console.log(i)
     }
+}*/
+/*const calculate = (x,y) => {
+	return 2 * x + y;
 }
+console.log(calculate(5,3));*/
+
+/*console.log(numbers.reduce((a, b) => a + b));*/
+//
+const marks = [7, 3, 7, 8, 9, 10];
+let sum = 0, average = 0;
+for (let i = 0; i < marks.length; sum += marks[i++]);
+	console.log('Suma notelor este :' + sum);
+average = sum / marks.length;
+	console.log('Media este :' + average);
+min = Math.min(...marks);
+	console.log('Nota minima este : ' + min);
+if (min < 5) {
+    console.log('Studentul este restantier.');
+} 
+else console.log('Studentul nu este restantier.');
+
