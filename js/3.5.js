d4d3461dc403633f80d2f878de0509fd50eb1e91
Theodor <54949643+theodorerlax@users.noreply.github.com>
2021-02-25 08:41:22 +0200

    Delete 3.5.js

--- js/3.5.js
+++
@@ -1,9 +0,0 @@
-square = value => Math.pow(2, value);
-
-convert = (square, array)  => {
-   return array.map(square)
-};
-
-let arr = [1, 2, 3];
-console.log(convert(square, arr)); 
-console.log(arr); 
